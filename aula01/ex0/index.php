1b053b22a3eb0b80bfb8dd730512c8b6d3947962
jgabsx <joao.gsl@puccampinas.edu.br>
2024-06-24 01:34:35 -0300

    Aula 01 - PHP Moderno

---
+++ aula01/ex0/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Course</title>
+</head>
+<body>
+    <!-- print -->
+    <h1>
+        <?php 
+            print "Olá mundo! \u{1F30E}";
+        ?>
+    </h1>
+    <!-- echo -->
+    <h1>
+        <?php 
+            echo "Olá mundo! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Course in Vídeo</p>
+</body>
+</html>
